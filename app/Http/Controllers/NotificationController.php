f3538aafa1e29e64b0e75068a8df78da79ebbdf6
luckykun88 <kmisigaro@outlook.com>
2022-05-31 18:16:17 +0300

    changes

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Notification;
+
+class NotificationController extends Controller
+{
+    public function sendNotification(Request $request){
+        
+    }
+}
